c2871b63a5e7f28cfdd418e01301e4cf8b5b52dc
aadisp <spaadi1601@gmail.com>
2026-06-14 20:49:59 +0530

    Delete Python/Intermediate directory

--- Python/Intermediate/2 Quiz Project/question_model.py
+++
@@ -1,9 +0,0 @@
-# class Question:
-#     def __init__(self,txt,ans):
-#         self.text=txt
-#         self.answer=ans
-
-class Question:
-    def __init__(self,txt,ans):
-        self.text=txt
-        self.answer=ans
